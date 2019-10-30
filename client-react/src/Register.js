e6dbb07f2a6b24733f02a2a4f0de4a68ee31cc6b
Donfrancis Osaji <donfrancis89@gmail.com>
2019-10-30 14:59:39 -0400

    Register page created and Routed

---
+++ client-react/src/Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Register</h2>
+        <p>Blog Articles goes here</p>
+        <ul>
+         
+        </ul>
+      </div>
+    );
+  }
+}
+ 
+export default Register;
