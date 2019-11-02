f8e57cad51db7ba469f0f34ae5289f36bbb36e33
Donfrancis Osaji <donfrancis89@gmail.com>
2019-11-02 18:17:36 -0400

    Forgot password form created

--- client-react/src/Forgotpass.js
+++ client-react/src/Forgotpass.js
@@ -1,17 +1,46 @@
-import React, { Component } from "react";
- 
-class Forgotpass extends Component {
+import React from "react";
+import { Form, Field } from 'react-advanced-form'
+import { Input, Button } from 'react-advanced-form-addons'
+
+ export default class Forgotpass extends React.Component {
+  registerUser = ({ serialized, fields, form }) => {
+    return fetch('https://backend.dev', {
+      body: JSON.stringify(serialized)
+    })
+  }
+  
   render() {
     return (
+
       <div>
         <h2>Forgot Password</h2>
-        <p>Blog Articles goes here</p>
+        <p>
+         
+
+      <Form
+        action={this.registerUser}
+        onSubmitStart={this.props.onSubmitStart}>
+        <Field.Group name="primaryInfo">
+          <Input
+            name="userEmail"
+            type="email"
+            label="E-mail Address"
+            placeholder="Email Address"
+            required />
+        </Field.Group>
+
+       
+       
+        <Button primary>Reset Password</Button>
+      </Form>       
+
+              <br/> <br/>
+        </p>
         <ul>
          
         </ul>
       </div>
+
     );
   }
 }
- 
-export default Forgotpass;
