7bd895f92837a06b434ba017ac8dec23af9435c8
Len Rundblad <Lrundblad@gmail.com>
2019-11-13 18:51:05 -0600

    Stylizing

--- client-react/src/Tips.js
+++ client-react/src/Tips.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from 'axios';
 
-class Tips extends React.Component {
+class Tips extends Component {
   constructor(props) {
     super(props);
     this.state = { projects: [] };
@@ -17,12 +17,6 @@ class Tips extends React.Component {
   getData = () => {
     // Java Spring Boot uses port 8080
     let url = "http://localhost:8080/tips";
-
-    // C# dotnetcore uses port 5000
-    //let url = "http://localhost:5000/projects";
-
-    // Express uses port 3001 (react uses 3000)
-    //let url = "http://localhost:3001/tasks";
     axios.get(url).then(response => this.setState({ projects: response.data }));
   };
 
@@ -37,88 +31,117 @@ class Tips extends React.Component {
     });
   };
 
-  
+
 
   render() {
     return (
-      <div>
-        <div className="container">
-          <div className="jumbotron text-center">
-            <h1><b>Tips Page</b></h1>
-            <h4>Welcome to our Tips page, everything from items to training our companion.</h4>
-          </div>
+      <div className="container">
+        <div className="jumbotron text-center">
+          <h1><b>Tips Page</b></h1>
+          <h4>Welcome to our Tips page, everything from items to training your companion.</h4>
+        </div>
 
-          <div className="row mb-2">
-            <div className="col-md-6">
-              <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-md h-md-250 position-relative">
-                <div className="col p-4 d-flex-column position-static">
-                  <h2>Our Item of the week!</h2>
-                  <h3>The Hurtta Trail Pack</h3>
-                  <img className="img-fluid" alt="DogPack" src="https://images-na.ssl-images-amazon.com/images/I/61qFOmXnYHL._AC_SL1000_.jpg"></img>
-                  <p>This is the best all in one bag for dog walking. You don't even need to carry the bag,
-                    the harness can be equiped with bags that go on either side of the dog.
-                    So you can leave the lifting for your little companion. Enough room to store all your dog walking needs.
-                </p>
-                </div>
-              </div>
-            </div>
+        <div className="row mb-2">
 
-            <div className="col-md-6">
-              <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-md h-md-250 position-relative">
-                <div className="col p-4 d-flex-column position-static">
-                  <h3><b>Todays activity for your dog!</b></h3>
-                  <h3>To the Park!</h3>
-                  <img className="img-fluid" alt="DogsPark" src="https://www.wellnesspetfood.com/sites/default/files/styles/blog_feature/public/media/images/dog-park.jpg?itok=O8vkW9-R"></img>
-                  <p>Okay we get it, you just got home from work and definitely dont want to go back out.
-                    But who loves to greet you everyday once you get home, who loves meeting your friends when they come over.
-                    Your dog. They LOVE to socialize and interact with people and animals. The dog park is a great way for your pup
-                    to get some exercise and familiarize themselves with other dogs.
-              </p>
-                </div>
-              </div>
-              <h2><b>Video of the week!</b></h2>
-              <iframe width="560" height="315"
-                src="https://www.youtube.com/embed/zye3DqVNHu0"
-                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-              </iframe>
-            </div>
-          </div>
+          {/* Item of week */}
 
-          <div className="col-lg-12">
+          <div className="col-md-6">
             <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-md h-md-250 position-relative">
-              <label>
-                <input ref={this.userName} placeholder="Username" />
-              </label>
-              <label>
-                <input ref={this.tipsSubmission} placeholder="Enter your tip here!" />
-              </label>
-              <label>
-                <button onClick={this.addTip}>Add Tip</button>
-              </label>
-              <h3>List of Users in Our Database</h3>
-              <table >
-                <div>
-                  <th>ID**</th>
-                  <th>UserName</th>
-                  <th>tipsSubmission</th>
-                </div>
-                <div>
-                  {this.state.projects.map(p => (
-                    <tr key={p.id}>
-                      <td>{p.id}**</td>
-                      <td>{p.userName} </td>
-                      <td>{p.tipsSubmission} </td>
-                    </tr>
-                  ))}</div>
-              </table>
-              
+              <div className="col p-4 d-flex-column position-static">
+                <h2>Our Item of the week!</h2>
+                <h3>The Hurtta Trail Pack</h3>
+                <img className="img-fluid" alt="DogPack" src="https://images-na.ssl-images-amazon.com/images/I/61qFOmXnYHL._AC_SL1000_.jpg"></img>
+                <p>This is the best all in one bag for dog walking. You don't even need to carry the bag,
+                  the harness can be equiped with bags that go on either side of the dog.
+                  So you can leave the lifting for your little companion. Enough room to store all your dog walking needs.
+                </p>
+              </div>
             </div>
+
+            {/* Tip Form */}
+
+            <div className="border-right">
+              <div className="card">
+                <h4 className="card-header info-color white-text text-center py-4">Got a Tip we missed? Submit it below!</h4>
+                <label for="matematerialTipFormUsername">
+                  <input
+                    type="text"
+                    id="materialTipFormUsername"
+                    className="form-control"
+                    ref={this.userName}
+                    placeholder="Username" />
+                </label>
+                <label for="matematerialTipFormUsername">
+                  <textarea
+                    id="matematerialTipFormUsername"
+                    className="form-control md-textarea"
+                    rows="5"
+                    ref={this.tipsSubmission}
+                    placeholder="Enter your tip here!">
+                  </textarea>
+                </label>
+                <button className="btn btn-primary btn-rounded btn-block z-depth-0 my-4 waves-effect" onClick={this.addTip}>Add Tip</button>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="col-md-6">
+
+            {/* Activity for dog */}
+
+            <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-md h-md-250 position-relative">
+              <div className="col p-4 d-flex-column position-static">
+                <h3><b>Todays activity for your dog!</b></h3>
+                <h3>To the Park!</h3>
+                <img className="img-fluid" alt="DogsPark" src="https://www.wellnesspetfood.com/sites/default/files/styles/blog_feature/public/media/images/dog-park.jpg?itok=O8vkW9-R"></img>
+                <p>Okay we get it, you just got home from work and definitely dont want to go back out.
+                  But who loves to greet you everyday once you get home, who loves meeting your friends when they come over.
+                  Your dog. They LOVE to socialize and interact with people and animals. The dog park is a great way for your pup
+                  to get some exercise and familiarize themselves with other dogs.
+              </p>
+              </div>
+            </div>
+            <h2><b>Video of the week!</b></h2>
+            <iframe width="560" height="315"
+              src="https://www.youtube.com/embed/zye3DqVNHu0"
+              frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen>
+            </iframe>
           </div>
         </div>
-        
+
+        <div className="row">
+
+          {/* Tip List */}
+
+          <div className="col-12 bg-grey">
+            <h4 className="card-header info-color white-text text-center py-4">Tips from people like you!</h4>
+            <table>
+              <div>
+                <th>Username</th>
+                <th>tipsSubmission</th>
+              </div>
+              <div>
+                {this.state.projects.map(p => (
+                  <tr key={p.id}>
+                    <div className="card">
+                      <div className="card-header white">
+                        <td>{p.userName} </td>
+                      </div>
+                      <div className="card-body">
+                        <td>{p.tipsSubmission} </td>
+                      </div>
+                    </div>
+                  </tr>
+                ))}
+              </div>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 export default Tips;
